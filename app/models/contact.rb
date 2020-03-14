4bf4ccca7241e53bac18cbac112b20d303d11767
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-14 16:23:26 +0530

    Add tracking Type

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ApplicationRecord
   enum sex: [:male, :female]
   enum district: [:alappuzha, :ernakulam, :idukki, :kannur, :kasargod, :kollam, :kottayam, :kozhikode, :malappuram, :palakkad, :pathanamthitta, :thiruvananthapuram, :thrissur, :wayanad]
   enum risk: [:high, :medium ,:low]
+  enum tracking_type: [:primary, :secondary, :patient]
   enum previous_medical: []
   enum symptoms: []
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_103217) do
+ActiveRecord::Schema.define(version: 2020_03_14_104948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_03_14_103217) do
     t.integer "symptoms"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "tracking_type"
+    t.integer "integer"
     t.index ["patient_id"], name: "index_contacts_on_patient_id", unique: true
   end
 
