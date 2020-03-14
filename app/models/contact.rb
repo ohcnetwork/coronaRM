6175ef2f9b5d77e94b01e98f77dce71496078cfe
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-14 13:38:23 +0530

    Change enum to readbale data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   enum sex: [:male, :female]
   enum district: [:alappuzha, :ernakulam, :idukki, :kannur, :kasargod, :kollam, :kottayam, :kozhikode, :malappuram, :palakkad, :pathanamthitt, :thiruvananthapuram, :thrissur, :wayanad]
-  enum risk: [:high_risk, :low_risk, :no_risk]
+  enum risk: [:high, :medium ,:low]
   enum previous_medical: []
   enum symptoms: []
 end
