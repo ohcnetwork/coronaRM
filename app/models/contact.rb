c685dd0c1a614bb801a66c76733d6e0d2d74c595
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-14 14:34:16 +0530

    Fix typo at pathanamthitta

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   enum sex: [:male, :female]
-  enum district: [:alappuzha, :ernakulam, :idukki, :kannur, :kasargod, :kollam, :kottayam, :kozhikode, :malappuram, :palakkad, :pathanamthitt, :thiruvananthapuram, :thrissur, :wayanad]
+  enum district: [:alappuzha, :ernakulam, :idukki, :kannur, :kasargod, :kollam, :kottayam, :kozhikode, :malappuram, :palakkad, :pathanamthitta, :thiruvananthapuram, :thrissur, :wayanad]
   enum risk: [:high, :medium ,:low]
   enum previous_medical: []
   enum symptoms: []
