8bac2f1b4a9f5e09f054562ccd41b0fbe6e61b5d
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-14 12:55:32 +0530

    Run Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_14_072356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.integer "patient_id"
+    t.string "phone"
+    t.integer "age"
+    t.integer "sex"
+    t.string "house_name"
+    t.integer "ward"
+    t.string "panchayath"
+    t.string "landmark"
+    t.integer "district"
+    t.integer "risk"
+    t.integer "previous_medical"
+    t.integer "symptoms"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_contacts_on_patient_id", unique: true
+  end
 
 end
