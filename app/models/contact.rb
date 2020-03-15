b03eabc468383f22d34c0ed657553ea1cd7b3ba5
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-15 23:29:27 +0530

    Fix Id Typo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,6 @@ class Contact < ApplicationRecord
   DISTRICTS_IN_KERALA = %w[alappuzha ernakulam idukki kannur kasargod kollam kottayam kozhikode malappuram palakkad pathanamthitta thiruvananthapuram thrissur wayanad].freeze
   enum district: DISTRICTS_IN_KERALA.zip(DISTRICTS_IN_KERALA).to_h
 
-  has_many :infectees, class_name: 'Contact', foreign_key: 'infectee_id'
+  has_many :infectees, class_name: 'Contact', foreign_key: 'infector_id'
   belongs_to :infector, class_name: 'Contact', optional: true
 end
