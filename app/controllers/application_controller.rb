5886ef63a970f1ae7c92162a6c30ba4ce3421d77
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-16 14:59:13 +0530

    Readd authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"], if: -> { ENV['RAILS_ENV'] == 'production' }
 end
