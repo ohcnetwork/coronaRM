ef76aeb150c309ded94895dd49470c4f6b545871
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-16 12:54:35 +0530

    Yeah Yeah Yeah

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
 
   # GET /contacts
