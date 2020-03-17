76b5e5dc01cf4417ae8b52579926178ab3c0d107
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-17 07:58:06 +0530

    Add Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:user, :caller, :admin_district, :medical_district, :super_district, :admin_state, :medical_state, :super_district]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
