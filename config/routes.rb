e7bc562e163f4ee4b8bbab834173b9350489d668
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-17 16:31:28 +0530

    Fix Error

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'contacts#index'
-  devise_for :users, :skip => [:registrations], controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
 
   resources :contacts, except: [:destroy] do
     get :make_call, on: :member
