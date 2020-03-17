44a9d06c1470ab81de2909127323ed6f3168e97a
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-17 17:22:32 +0530

    Seeding Data and Update Data Display

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,30 @@
 
 # Load the patient Data
 require 'csv'
+
+# Check if Date is Valid
+def valid_date_or_empty_string(date)
+  date_format = '%m/%d/%Y'
+  date = DateTime.strptime(date, date_format)
+  date
+rescue ArgumentError
+  nil
+end
+
 CSV.foreach(Rails.root.join('data/initial_data.csv'), headers: true) do |row|
   Contact.create({
                   patient_id: row[0] ,
                   name: row[1],
-                  sex: row[2] && row[2] == "Male" ? :male : :female,
-                  age: row[3],
-                  house_name: row[4],
-                  panchayath: row[5],
-                  phone: row[6],
-                  tracking_type: row[7] && row[7] == "Primary" ? :primary : :secondary,
-                  risk: row[8] &&  row[8].downcase.include?("High") ? :high : :low
+                  tracking_type: :primary,
+                  gender: row[8].first.downcase == 'm' ? :male : :female,
+                  age: row[9],
+                  phc_name: row[10],
+                  house_name: row[11],
+                  town: row[12],
+                  district: row[13].downcase.strip,
+                  phone: row[14],
+                  date_of_first_contact: valid_date_or_empty_string(row[16] ? row[16] : ""),
+                  mode_of_contact: row[19],
+                  risk: row[20]&.first&.downcase == 'h' ? :high : :low
                  })
 end
