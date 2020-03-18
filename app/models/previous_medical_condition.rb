c7bfa4440b2d14aa1b23474796c31f1bbd4f8032
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-18 10:14:48 +0530

    Add Previous History Data

--- app/models/previous_medical_condition.rb
+++ app/models/previous_medical_condition.rb
@@ -1,4 +1,4 @@
 class PreviousMedicalCondition < ApplicationRecord
-  enum condition_type: {diabetes: "diabetes"}
+  enum condition_type: {diabetes: "diabetes", hypertension: "hypertension", cardiac_issues: "cardiac_issues", cholestrol: "cholestrol", kidney_issues: "kidney_issues", stroke: "stroke", tubercolosis: "tubercolosis", others: "others"}
   belongs_to :contact
 end
