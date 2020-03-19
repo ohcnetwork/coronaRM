c85cbaf0ed3deaa7421a0b90d9df43b865e03c41
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-19 13:02:22 +0530

    Add Is tracking Field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_033932) do
+ActiveRecord::Schema.define(version: 2020_03_19_072336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_033932) do
     t.string "connecting_flight_details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_health_worker"
     t.index ["contact_id"], name: "index_flight_details_on_contact_id"
   end
 
