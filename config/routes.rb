887328b7c7140b4d3b68fbefdadb9581a140afb4
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-20 11:51:50 +0530

    Set Dashboard as Root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'contacts#index'
+  root to: 'dashboard#index'
   devise_for :users, :controllers => { :registrations => 'registrations'}
 
   resources :dashboard, only: [:index] do
