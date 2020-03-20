49e5927be8b1c39f423fe844376b9f190d8d5af8
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-20 13:23:04 +0530

    Fix Breaking Stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'dashboard#index'
+  root to: 'contacts#index'
   devise_for :users, :controllers => { :registrations => 'registrations'}
 
   resources :dashboard, only: [:index] do
