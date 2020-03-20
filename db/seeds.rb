41edd0115ede53fdd61d3d36cd2b5e156890fa56
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-20 11:17:42 +0530

    Import second phase of traveller data

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,8 @@ def create_districts
 end
 
 # Check if Date is Valid
-def valid_date_or_empty_string(date)
-  date_format = '%m/%d/%Y'
-  date = DateTime.strptime(date, date_format)
+def valid_date_or_empty_string(date, format)
+  date = DateTime.strptime(date,format)
   date
 rescue ArgumentError
   nil
@@ -40,7 +39,7 @@ def populate_patients_from_data_file
                       town: row[12],
                       district_id: District.find_by(name: row[13].titleize.strip).id,
                       phone: row[14],
-                      date_of_first_contact: valid_date_or_empty_string(row[16] ? row[16] : ""),
+                      date_of_first_contact: valid_date_or_empty_string(row[16] ? row[16] : "", "%d/%m/%Y") || valid_date_or_empty_string(row[16] ? row[16] : "", "%d.%m.%Y"),
                       mode_of_contact: row[19],
                       risk: row[20]&.first&.downcase == 'h' ? :high : :low
                     })
@@ -49,21 +48,27 @@ end
 
 
 def populate_traveller_from_data_file
-  CSV.foreach(Rails.root.join('data/traveller.csv'), headers: true) do |row|
+  CSV.foreach(Rails.root.join('data/traveller_2.csv'), headers: true) do |row|
     contact = Contact.create({
                      name: row[0],
-                     phone: row[1],
-                     house_name: row[2],
-                     ward: row[3],
-                     panchayath: row[4],
-                     phc_name: row[6],
-                     field_staff_name: row[7],
-                     field_staff_phone: row[8],
+                     age: row[1],
+                     gender: row[2] ? (row[2].first.downcase == 'm' ? :male : :female):  nil,
+                     phone: row[3],
+                     house_name: row[4],
+                     ward: row[5],
+                     panchayath: row[6],
+                     phc_name: row[7],
+                     field_staff_name: row[8],
+                     field_staff_phone: row[9],
+                     isolation_type: row[10] ? ((row[10].first.downcase == 'y' or row[10].first.downcase == 'h') ? :home_isolation : nil) : nil,
                      tracking_type: "flight_passenger",
                      district_id: District.find_by(name: "Pathanamthitta").id
                    })
     flight = contact.build_flight_detail({
-                                           arrival_airport: row[5],
+                                           date_of_arrival: valid_date_or_empty_string(row[11] ? row[11] : "", "%d/%m/%Y") || valid_date_or_empty_string(row[11] ? row[11] : "", "%d.%m.%Y"),
+                                           flight_number: row[12],
+                                           arrival_airport: row[13],
+                                           connecting_flight_details: row[14],
                                          })
     flight.save!
   end
