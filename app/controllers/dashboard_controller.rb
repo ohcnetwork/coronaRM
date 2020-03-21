e9cad54863a36c5a99114e8fb53c7e0501a8e86d
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-21 10:34:39 +0530

    Add Report for Non Contacted

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -18,6 +18,7 @@ class DashboardController < ApplicationController
     today = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
     @number_of_symptomatics_today = Contact.joins(:calls, :symptoms).where(calls:{created_at: today}).where({symptoms: {created_at: today}}).where.not(symptoms: {symptom_type: ""}).distinct.count
     @number_of_symptomatics = Contact.joins(:symptoms).where.not(symptoms: nil).where.not(symptoms: {symptom_type: ""}).distinct.count
+    @number_of_non_contacted_ever = @contacts_count - Contact.joins(:calls).distinct.count
   end
 
   def csv_report
