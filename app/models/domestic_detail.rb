c56ece206bd49bd15115e5ab1cf25e697af28319
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-25 08:36:29 +0530

    Add domestic flights

--- app/models/domestic_detail.rb
+++ app/models/domestic_detail.rb
@@ -1,4 +1,4 @@
 class DomesticDetail < ApplicationRecord
-  enum mode_of_transport: {train: "train", bus: "bus", private_vehicle: "private_vehicle"}
+  enum mode_of_transport: {train: "train", bus: "bus", private_vehicle: "private_vehicle", domestic_flight: "domestic_flight"}
   belongs_to :contact
 end
